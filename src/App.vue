64b701c57f3b0ce0fc52488ac587ccb94c3b41b8
Jaime Gonzalez <lnsflive@gmail.com>
2022-03-24 22:04:23 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <div id="logo">
+      <img alt="Metopio logo" src="./assets/brand.png" />
+    </div>
+    <TopicsDictionary />
+  </div>
+</template>
+
+<script>
+import TopicsDictionary from "./components/TopicsDictionary.vue";
+
+export default {
+  name: "App",
+  components: {
+    TopicsDictionary,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+#logo {
+  top: 0;
+  right: 0;
+}
+#logo img {
+  max-width: 15rem;
+  height: 5rem;
+  object-fit: contain;
+}
+</style>
---
+++ src/components/TopicsDictionary.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <div id="table-container">
+    <h1>Topics Dictionary</h1>
+    <p>
+      Visit
+      <a
+        href="https://chicagohealthatlas.org/api/v1/"
+        title="Chicago Health Atlas API page"
+        target="_blank"
+        rel="noopener noreferrer"
+        >chicagohealthatlas.org/api/v1/</a
+      >
+      to learn more about Chicago Health Atlas API
+    </p>
+    <h3>Your table goes here.</h3>
+  </div>
+</template>
+
+<script>
+// Please read the INSTRUCTIONS.md file
+export default {
+  name: "TopicsDictionary",
+};
+</script>
+
+<style scoped>
+#table-container {
+  text-align: center;
+  margin: 2rem;
+}
+</style>
